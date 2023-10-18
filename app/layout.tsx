22b18b0ad46fe3a82b93f568dad32d51d7d732a9
Jonas Gedrat <jonasgedrat@gmail.com>
2023-10-18 15:35:18 -0300

    inicial

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next frontEnd",
+  description: "The connected workspace where better, faster work happens.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="next-frontend-theme-2"
+        >
+          <ModalProvider />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <ModeToggle />
+      Page
+    </div>
+  );
+};
+
+export default Page;
